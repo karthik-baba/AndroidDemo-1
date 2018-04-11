bc3297c9d990401b5902510c5ddf2126a811301c
karthik-baba <karthikbaba.rules@gmail.com>
2018-04-11 20:43:17 +0530

    Initial commit

---
+++ app/src/androidTest/java/com/mytaxi/android_demo/TestMainActivity.java
@@ -0,0 +1,58 @@
+package com.mytaxi.android_demo;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.clearText;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.swipeUp;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.action.ViewActions.doubleClick;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasEntry;
+
+import com.mytaxi.android_demo.activities.MainActivity;
+
+@RunWith(AndroidJUnit4.class)
+public class TestMainActivity {
+    @Rule
+    public ActivityTestRule<MainActivity> activityActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+    @Before
+    public void init(){
+        activityActivityTestRule.getActivity()
+                .getSupportFragmentManager().beginTransaction();
+    }
+
+    @Test
+    public void InvalidLoginTest(){
+        onView(withId(R.id.edt_username)).perform(clearText(),typeText("karthik"));
+        onView(withId(R.id.edt_password)).perform(clearText(),typeText("testing"));
+       // onView(withId(R.id.btn_login)).perform(click());
+       // onView(withId(R.id.cast_notification_id))
+    }
+
+
+
+
+}
